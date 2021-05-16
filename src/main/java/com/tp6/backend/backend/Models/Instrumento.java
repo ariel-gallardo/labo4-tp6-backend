5ecfa62759551811648485437c6abc7bda1bca66
Ariel Gallardo <ariel.gallardo@alumnos.frm.utn.edu.ar>
2021-05-15 23:16:10 -0300

    development# cross fix - crud instrumento

---
+++ src/main/java/com/tp6/backend/backend/Models/Instrumento.java
@@ -0,0 +1,32 @@
+package com.tp6.backend.backend.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Instrumento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String instrumento,
+    marca,
+    modelo,
+    imagen,
+    precio,
+    costoEnvio;
+    private Long cantidadVendida;
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+}
